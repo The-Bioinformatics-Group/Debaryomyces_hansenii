3ca7ba421acd3918e8f2a30f6fb95060cd59714c
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-02-02 13:59:39 +0100

    Add groups pipeline to distinguish different strains

---
+++ Work_files/Only_Debaryomyceshansenii/Variant_calling/AddOrReplaceReadGroups/AddOrReplaceGroups_andIndex.sh
@@ -0,0 +1,50 @@
+!/usr/bin/env bash
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1001_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1001_AH_rmDUP_readGROUP.bam RGID=1001AH RGLB=1001AH9BY4ADXX RGPL=illumina RGPU=1001AH9BY4ADXX RGSM=1001AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1001_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1001_BC_rmDUP_readGROUP.bam RGID=1001BC RGLB=1001BC47HDACXX RGPL=illumina RGPU=1001BC47HDACXX RGSM=1001BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1002_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1002_AH_rmDUP_readGROUP.bam RGID=1002AH RGLB=1002AH9BY4ADXX RGPL=illumina RGPU=1002AH9BY4ADXX RGSM=1002AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1002_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1002_BC_rmDUP_readGROUP.bam RGID=1002BC RGLB=1002BC47HDACXX RGPL=illumina RGPU=1002BC47HDACXX RGSM=1002BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1003_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1003_AH_rmDUP_readGROUP.bam RGID=1003AH RGLB=1003AH9BY4ADXX RGPL=illumina RGPU=1003AH9BY4ADXX RGSM=1003AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1003_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1003_BC_rmDUP_readGROUP.bam RGID=1003BC RGLB=1003BC47HDACXX RGPL=illumina RGPU=1003BC47HDACXX RGSM=1003BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1004_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1004_AH_rmDUP_readGROUP.bam RGID=1004AH RGLB=1004AH9BY4ADXX RGPL=illumina RGPU=1004AH9BY4ADXX RGSM=1004AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1004_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1004_BC_rmDUP_readGROUP.bam RGID=1004BC RGLB=1004BC47HDACXX RGPL=illumina RGPU=1004BC47HDACXX RGSM=1004BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1005_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1005_AH_rmDUP_readGROUP.bam RGID=1005AH RGLB=1005AH9BY4ADXX RGPL=illumina RGPU=1005AH9BY4ADXX RGSM=1005AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1005_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1005_BC_rmDUP_readGROUP.bam RGID=1005BC RGLB=1005BC47HDACXX RGPL=illumina RGPU=1005BC47HDACXX RGSM=1005BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1007_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1007_AH_rmDUP_readGROUP.bam RGID=1007AH RGLB=1007AH9BY4ADXX RGPL=illumina RGPU=1007AH9BY4ADXX RGSM=1007AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1007_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1007_BC_rmDUP_readGROUP.bam RGID=1007BC RGLB=1007BC47HDACXX RGPL=illumina RGPU=1007BC47HDACXX RGSM=1007BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1008_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1008_AH_rmDUP_readGROUP.bam RGID=1008AH RGLB=1008AH9BY4ADXX RGPL=illumina RGPU=1008AH9BY4ADXX RGSM=1008AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1008_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1008_BC_rmDUP_readGROUP.bam RGID=1008BC RGLB=1008BC47HDACXX RGPL=illumina RGPU=1008BC47HDACXX RGSM=1008BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1013_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1013_AH_rmDUP_readGROUP.bam RGID=1013AH RGLB=1013AH9BY4ADXX RGPL=illumina RGPU=1013AH9BY4ADXX RGSM=1013AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1013_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1013_BC_rmDUP_readGROUP.bam RGID=1013BC RGLB=1013BC47HDACXX RGPL=illumina RGPU=1013BC47HDACXX RGSM=1013BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1014_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1014_AH_rmDUP_readGROUP.bam RGID=1014AH RGLB=1014AH9BY4ADXX RGPL=illumina RGPU=1014AH9BY4ADXX RGSM=1014AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1014_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1014_BC_rmDUP_readGROUP.bam RGID=1014BC RGLB=1014BC47HDACXX RGPL=illumina RGPU=1014BC47HDACXX RGSM=1014BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1015_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1015_AH_rmDUP_readGROUP.bam RGID=1015AH RGLB=1015AH9BY4ADXX RGPL=illumina RGPU=1015AH9BY4ADXX RGSM=1015AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1015_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1015_BC_rmDUP_readGROUP.bam RGID=1015BC RGLB=1015BC47HDACXX RGPL=illumina RGPU=1015BC47HDACXX RGSM=1015BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1016_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1016_AH_rmDUP_readGROUP.bam RGID=1016AH RGLB=1016AH9BY4ADXX RGPL=illumina RGPU=1016AH9BY4ADXX RGSM=1016AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1016_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1016_BC_rmDUP_readGROUP.bam RGID=1016BC RGLB=1016BC47HDACXX RGPL=illumina RGPU=1016BC47HDACXX RGSM=1016BC47HDACXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1019_AH_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1019_AH_rmDUP_readGROUP.bam RGID=1019AH RGLB=1019AH9BY4ADXX RGPL=illumina RGPU=1019AH9BY4ADXX RGSM=1019AH9BY4ADXX
+
+ java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/Only_Debaryomyceshansenii/1019_BC_rmDUP.bam OUTPUT=/data02/merce/Only_Debaryomyceshansenii/1019_BC_rmDUP_readGROUP.bam RGID=1019BC RGLB=1019BC47HDACXX RGPL=illumina RGPU=1019BC47HDACXX RGSM=1019BC47HDACXX
+
