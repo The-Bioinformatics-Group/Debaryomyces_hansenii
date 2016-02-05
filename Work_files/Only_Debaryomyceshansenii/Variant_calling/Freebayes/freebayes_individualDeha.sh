b0f8f99312fe6626ba7d5f5a4323376f31a3caa8
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-02-05 11:10:37 +0100

    bash for freebayes individual calling variants

---
+++ Work_files/Only_Debaryomyceshansenii/Variant_calling/Freebayes/freebayes_individualDeha.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1001_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1001_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1001_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1001_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1002_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1002_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1002_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1002_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1003_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1003_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1003_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1003_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1004_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1004_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1004_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1004_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1005_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1005_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1005_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1005_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1007_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1007_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1007_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1007_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1008_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1008_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1008_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1008_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1013_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1013_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1013_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1013_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1014_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1014_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1014_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1014_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1015_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1015_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1015_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1015_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1016_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1016_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1016_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1016_BC.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1019_AH_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1019_AH.vcf
+
+freebayes --fasta-reference reference/GCA_000006445.2_ASM644v2_genomic.fna --ploidy 1 --bam 1019_BC_rmDUP_readGROUP_BAQ.bam > Individual_freebayes/1019_BC.vcf
