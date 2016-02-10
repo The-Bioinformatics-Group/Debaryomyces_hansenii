7a24061ef9fcb392a8d59da93a099aa168e0eebf
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-02-10 13:36:58 +0100

    repair wrong commands

--- Work_files/Only_Debaryomyceshansenii/Variant_calling/post_varscan/snpEff_varscan.sh
+++ Work_files/Only_Debaryomyceshansenii/Variant_calling/post_varscan/snpEff_varscan.sh
@@ -2,6 +2,6 @@
 
 java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr Deha_varscan_snp_mpileup_finalvcf.vcf > Deha_varscan_snp_mpileup_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr varscan_deha_indel_mpileup.vcf > Deha_varscan_snp_mpileup_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr varscan_deha_indel_mpileup_finalvcf.vcf > Deha_varscan_indel_mpileup_snpEff.vcf
 
 
