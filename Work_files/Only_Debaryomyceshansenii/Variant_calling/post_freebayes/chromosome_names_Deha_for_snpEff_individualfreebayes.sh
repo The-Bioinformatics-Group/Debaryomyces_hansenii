d34bd775621a94cd334b90dfc921b865dbe1da7b
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-02-10 13:25:16 +0100

    solve sed problem

--- Work_files/Only_Debaryomyceshansenii/Variant_calling/post_freebayes/chromosome_names_Deha_for_snpEff_individualfreebayes.sh
+++ Work_files/Only_Debaryomyceshansenii/Variant_calling/post_freebayes/chromosome_names_Deha_for_snpEff_individualfreebayes.sh
@@ -1,48 +1,48 @@
 #!/usr/bin/env bash
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1001_AH.vcf > 1001_AH_finalvcf.vcf 
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1001_AH.vcf > 1001_AH_finalvcf.vcf 
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1001_BC.vcf > 1001_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1001_BC.vcf > 1001_BC_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1002_AH.vcf > 1002_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1002_AH.vcf > 1002_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1002_BC.vcf > 1002_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1002_BC.vcf > 1002_BC_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1003_AH.vcf > 1003_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1003_AH.vcf > 1003_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1003_BC.vcf > 1003_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1003_BC.vcf > 1003_BC_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1004_AH.vcf > 1004_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1004_AH.vcf > 1004_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1004_BC.vcf > 1004_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1004_BC.vcf > 1004_BC_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1005_AH.vcf > 1005_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1005_AH.vcf > 1005_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1005_BC.vcf > 1005_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1005_BC.vcf > 1005_BC_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1007_AH.vcf > 1007_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1007_AH.vcf > 1007_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1007_BC.vcf > 1007_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1007_BC.vcf > 1007_BC_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1008_AH.vcf > 1008_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1008_AH.vcf > 1008_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1008_BC.vcf > 1008_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1008_BC.vcf > 1008_BC_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1013_AH.vcf > 1013_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1013_AH.vcf > 1013_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1013_BC.vcf > 1013_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1013_BC.vcf > 1013_BC_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1014_AH.vcf > 1014_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1014_AH.vcf > 1014_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1014_BC.vcf > 1014_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1014_BC.vcf > 1014_BC_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1015_AH.vcf > 1015_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1015_AH.vcf > 1015_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1015_BC.vcf > 1015_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1015_BC.vcf > 1015_BC_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1016_AH.vcf > 1016_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1016_AH.vcf > 1016_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1019_AH.vcf > 1019_AH_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1019_AH.vcf > 1019_AH_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/' -e 's/CR382134.2/B/' -e 's/CR382135.2/C/' -e 's/CR382136.2/CR382136/' -e 's/CR382137.2/E/' -e 's/CR382138.2/F/' -e 's/CR382139.2/G/' 1019_BC.vcf > 1019_BC_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' 1019_BC.vcf > 1019_BC_finalvcf.vcf
 
