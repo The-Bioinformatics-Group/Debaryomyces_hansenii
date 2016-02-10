4dfd65b98909bd136667439e8a1e9eed7e98a9f3
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-02-10 14:08:12 +0100

    change stats files names to not rewrite

--- Work_files/Only_Debaryomyceshansenii/Variant_calling/post_freebayes/vcffilter_and_snpEff_individualfreebayes.sh
+++ Work_files/Only_Debaryomyceshansenii/Variant_calling/post_freebayes/vcffilter_and_snpEff_individualfreebayes.sh
@@ -52,50 +52,50 @@
 
 #snpEff:
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1001_AH_10DPQRg.vcf > 1001AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1001ah_stats.html 1001_AH_10DPQRg.vcf > 1001AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1001_BC_10DPQRg.vcf > 1001BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1001bc_stats.html 1001_BC_10DPQRg.vcf > 1001BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1002_AH_10DPQRg.vcf > 1002AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1002ah_stats.html 1002_AH_10DPQRg.vcf > 1002AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1002_BC_10DPQRg.vcf > 1002BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1002bc_stats.html 1002_BC_10DPQRg.vcf > 1002BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1003_AH_10DPQRg.vcf > 1003AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1003ah_stats.html 1003_AH_10DPQRg.vcf > 1003AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1003_BC_10DPQRg.vcf > 1003BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1003bc_stats.html 1003_BC_10DPQRg.vcf > 1003BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1004_AH_10DPQRg.vcf > 1004AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1004ah_stats.html 1004_AH_10DPQRg.vcf > 1004AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1004_BC_10DPQRg.vcf > 1004BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1004bc_stats.html 1004_BC_10DPQRg.vcf > 1004BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1005_AH_10DPQRg.vcf > 1005AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1005ah_stats.html 1005_AH_10DPQRg.vcf > 1005AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1005_BC_10DPQRg.vcf > 1005BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1005bc_stats.html 1005_BC_10DPQRg.vcf > 1005BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1007_AH_10DPQRg.vcf > 1007AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1007ah_stats.html 1007_AH_10DPQRg.vcf > 1007AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1007_BC_10DPQRg.vcf > 1007BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1007bc_stats.html 1007_BC_10DPQRg.vcf > 1007BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1008_AH_10DPQRg.vcf > 1008AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1008ah_stats.html 1008_AH_10DPQRg.vcf > 1008AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1008_BC_10DPQRg.vcf > 1008BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1008bc_stats.html 1008_BC_10DPQRg.vcf > 1008BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1013_AH_10DPQRg.vcf > 1013AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1013ah_stats.html 1013_AH_10DPQRg.vcf > 1013AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1013_BC_10DPQRg.vcf > 1013BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1013bc_stats.html 1013_BC_10DPQRg.vcf > 1013BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1014_AH_10DPQRg.vcf > 1014AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1014ah_stats.html 1014_AH_10DPQRg.vcf > 1014AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1014_BC_10DPQRg.vcf > 1014BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1014bc_stats.html 1014_BC_10DPQRg.vcf > 1014BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1015_AH_10DPQRg.vcf > 1015AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1015ah_stats.html 1015_AH_10DPQRg.vcf > 1015AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1015_BC_10DPQRg.vcf > 1015BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1015bc_stats.html 1015_BC_10DPQRg.vcf > 1015BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1016_AH_10DPQRg.vcf > 1016AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1016ah_stats.html 1016_AH_10DPQRg.vcf > 1016AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1016_BC_10DPQRg.vcf > 1016BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1016bc_stats.html 1016_BC_10DPQRg.vcf > 1016BC_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1019_AH_10DPQRg.vcf > 1019AH_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1019ah_stats.html 1019_AH_10DPQRg.vcf > 1019AH_freebayes_individual_filtered_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr 1019_BC_10DPQRg.vcf > 1019BC_freebayes_individual_filtered_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats 1019bc_stats.html 1019_BC_10DPQRg.vcf > 1019BC_freebayes_individual_filtered_snpEff.vcf
