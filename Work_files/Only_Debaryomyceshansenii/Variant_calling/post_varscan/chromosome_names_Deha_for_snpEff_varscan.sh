670fd5987b1bdc2e1c59952e38490fbf1c641f7e
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-02-10 14:21:26 +0100

    more stupid errors corrected

--- Work_files/Only_Debaryomyceshansenii/Variant_calling/post_varscan/chromosome_names_Deha_for_snpEff_varscan.sh
+++ Work_files/Only_Debaryomyceshansenii/Variant_calling/post_varscan/chromosome_names_Deha_for_snpEff_varscan.sh
@@ -6,7 +6,7 @@ sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 
 
 sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' varscan_deha_snp_mpileupBAQ.vcf > Deha_varscan_snp_mpileupBAQ_finalvcf.vcf
 
-sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2g/G/g' varscan_deha_snp_mpileup.vcf > Deha_varscan_snp_mpileup_finalvcf.vcf
+sed -e 's/CR382133.2/CR382133/g' -e 's/CR382134.2/B/g' -e 's/CR382135.2/C/g' -e 's/CR382136.2/CR382136/g' -e 's/CR382137.2/E/g' -e 's/CR382138.2/F/g' -e 's/CR382139.2/G/g' varscan_deha_snp_mpileup.vcf > Deha_varscan_snp_mpileup_finalvcf.vcf
 
 
 
