264e3e82da64402bb60d47b814daa5b20d391992
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-02-10 14:14:56 +0100

    and correcting more errors...

--- Work_files/Only_Debaryomyceshansenii/Variant_calling/post_varscan/snpEff_varscan.sh
+++ Work_files/Only_Debaryomyceshansenii/Variant_calling/post_varscan/snpEff_varscan.sh
@@ -2,6 +2,7 @@
 
 java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats snp_varscan_stats.html Deha_varscan_snp_mpileup_finalvcf.vcf > Deha_varscan_snp_mpileup_snpEff.vcf
 
-java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats indel_varscan_stats.html Deha_varscan_indel_mpileup_snpEff.vcf > Deha_varscan_indel_mpileup_snpEff.vcf
+java -jar ~/snpEff/snpEff.jar -v GCA_000006445.2.29 -no-downstream -no-intergenic -no-intron -no-upstream -no-utr -stats indel_varscan_stats.html Deha_indel_varscan_mpileup_finalvcf.vcf > Deha_varscan_indel_mpileup_snpEff.vcf
+
 
 
