dbf4ac4c25e2b2999e3f3c01e42e44e8c5c09cf0
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-03-21 10:05:24 +0100

    join libraries

---
+++ Work_files/Only_Debaryomyceshansenii/Variant_calling_4/2_cat.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+cat ~/raw_reads/1001AH_uniq1.fastq ~/raw_reads/1001BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1001_1.fastq
+
+cat ~/raw_reads/1001AH_uniq2.fastq ~/raw_reads/1001BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1001_2.fastq
+
+cat ~/raw_reads/1002AH_uniq1.fastq ~/raw_reads/1002BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1002_1.fastq
+
+cat ~/raw_reads/1002AH_uniq2.fastq ~/raw_reads/1002BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1002_2.fastq
+
+cat ~/raw_reads/1003AH_uniq1.fastq ~/raw_reads/1003BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1003_1.fastq
+
+cat ~/raw_reads/1003AH_uniq2.fastq ~/raw_reads/1003BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1003_2.fastq
+
+cat ~/raw_reads/1004AH_uniq1.fastq ~/raw_reads/1004BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1004_1.fastq
+
+cat ~/raw_reads/1004AH_uniq2.fastq ~/raw_reads/1004BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1004_2.fastq
+
+cat ~/raw_reads/1005AH_uniq1.fastq ~/raw_reads/1005BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1005_1.fastq
+
+cat ~/raw_reads/1005AH_uniq2.fastq ~/raw_reads/1005BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1005_2.fastq
+
+cat ~/raw_reads/1007AH_uniq1.fastq ~/raw_reads/1007BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1007_1.fastq
+
+cat ~/raw_reads/1007AH_uniq2.fastq ~/raw_reads/1007BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1007_2.fastq
+
+cat ~/raw_reads/1008AH_uniq1.fastq ~/raw_reads/1008BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1008_1.fastq
+
+cat ~/raw_reads/1008AH_uniq2.fastq ~/raw_reads/1008BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1008_2.fastq
+
+cat ~/raw_reads/1013AH_uniq1.fastq ~/raw_reads/1013BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1013_1.fastq
+
+cat ~/raw_reads/1013AH_uniq2.fastq ~/raw_reads/1013BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1013_2.fastq
+
+cat ~/raw_reads/1014AH_uniq1.fastq ~/raw_reads/1014BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1014_1.fastq
+
+cat ~/raw_reads/1014AH_uniq2.fastq ~/raw_reads/1014BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1014_2.fastq
+
+cat ~/raw_reads/1015AH_uniq1.fastq ~/raw_reads/1015BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1015_1.fastq
+
+cat ~/raw_reads/1015AH_uniq2.fastq ~/raw_reads/1015BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1015_2.fastq
+
+cat ~/raw_reads/1016AH_uniq1.fastq ~/raw_reads/1016BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1016_1.fastq
+
+cat ~/raw_reads/1016AH_uniq2.fastq ~/raw_reads/1016BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1016_2.fastq
+
+cat ~/raw_reads/1019AH_uniq1.fastq ~/raw_reads/1019BC_uniq1.fastq > /data02/merce/deha_vcalling/fastq_uniq/1019_1.fastq
+
+cat ~/raw_reads/1019AH_uniq2.fastq ~/raw_reads/1019BC_uniq2.fastq > /data02/merce/deha_vcalling/fastq_uniq/1019_2.fastq
+
