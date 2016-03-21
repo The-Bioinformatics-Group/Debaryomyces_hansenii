4b44c361054779b499f2fe2ff3d032fc521ba355
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-03-21 10:02:15 +0100

    remove pcr dupl from raw reads

---
+++ Work_files/Only_Debaryomyceshansenii/Variant_calling_4/1_pcrdupl.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+#/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1001_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1001_2.fastq -prefix=1001AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1002_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1002_2.fastq -prefix=1002AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1003_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1003_2.fastq -prefix=1003AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1004_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1004_2.fastq -prefix=1004AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1005_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1005_2.fastq -prefix=1005AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1007_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1007_2.fastq -prefix=1007AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1008_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1008_2.fastq -prefix=1008AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1013_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1013_2.fastq -prefix=1013AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1014_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1014_2.fastq -prefix=1014AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1015_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1015_2.fastq -prefix=1015AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1016_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1016_2.fastq -prefix=1016AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=1_140716_AH9BY4ADXX_P1260_1019_1.fastq -fastq2=1_140716_AH9BY4ADXX_P1260_1019_2.fastq -prefix=1019AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1001_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1001_2.fastq -prefix=1001BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1002_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1002_2.fastq -prefix=1002BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1003_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1003_2.fastq -prefix=1003BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1004_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1004_2.fastq -prefix=1004BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1005_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1005_2.fastq -prefix=1005BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1007_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1007_2.fastq -prefix=1007BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1008_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1008_2.fastq -prefix=1008BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1013_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1013_2.fastq -prefix=1013BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1014_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1014_2.fastq -prefix=1014BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1015_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1015_2.fastq -prefix=1015BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1016_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1016_2.fastq -prefix=1016BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=8_140619_BC47HDACXX_P1260_1019_1.fastq -fastq2=8_140619_BC47HDACXX_P1260_1019_2.fastq -prefix=1019BC
