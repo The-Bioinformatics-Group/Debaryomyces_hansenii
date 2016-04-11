5da9b335a6c0498be238f286326b79ffa5d32213
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-04-11 14:12:05 +0200

    pipeline - pcr duplicates and cat fastq together

---
+++ Project_pipeline/merge_fastq.sh
@@ -0,0 +1,143 @@
+#!/usr/bin/env bash
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1001_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1001_2.fastq -prefix=1001AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1002_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1002_2.fastq -prefix=1002AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1003_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1003_2.fastq -prefix=1003AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1004_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1004_2.fastq -prefix=1004AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1005_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1005_2.fastq -prefix=1005AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1007_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1007_2.fastq -prefix=1007AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1008_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1008_2.fastq -prefix=1008AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1013_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1013_2.fastq -prefix=1013AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1014_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1014_2.fastq -prefix=1014AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1015_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1015_2.fastq -prefix=1015AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1016_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1016_2.fastq -prefix=1016AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1019_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1019_2.fastq -prefix=1019AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1001_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1001_2.fastq -prefix=1001BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1002_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1002_2.fastq -prefix=1002BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1003_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1003_2.fastq -prefix=1003BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1004_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1004_2.fastq -prefix=1004BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1005_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1005_2.fastq -prefix=1005BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1007_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1007_2.fastq -prefix=1007BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1008_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1008_2.fastq -prefix=1008BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1013_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1013_2.fastq -prefix=1013BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1014_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1014_2.fastq -prefix=1014BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1015_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1015_2.fastq -prefix=1015BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1016_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1016_2.fastq -prefix=1016BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1019_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1019_2.fastq -prefix=1019BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1006_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1006_2.fastq -prefix=1006AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1009_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1009_2.fastq -prefix=1009AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1010_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1010_2.fastq -prefix=1010AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1011_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1011_2.fastq -prefix=1011AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1012_1.fastq -fastq2=/home/merce/raw_reads/1_140716_AH9BY4ADXX_P1260_1012_2.fastq -prefix=1012AH
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1006_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1006_2.fastq -prefix=1006BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1009_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1009_2.fastq -prefix=1009BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1010_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1010_2.fastq -prefix=1010BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1011_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1011_2.fastq -prefix=1011BC
+
+/usr/local/bin/filterPCRdupl_v1.01.pl -fastq1=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1012_1.fastq -fastq2=/home/merce/raw_reads/8_140619_BC47HDACXX_P1260_1012_2.fastq -prefix=1012BC
+
+
+mkdir /home/merce/raw_reads/fastq_uniq
+
+# cat them together:
+
+cat /home/merce/raw_reads/1006AH_uniq1.fastq /home/merce/raw_reads/1006BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1006_1.fastq
+
+cat /home/merce/raw_reads/1006AH_uniq2.fastq /home/merce/raw_reads/1006BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1006_2.fastq
+
+cat /home/merce/raw_reads/1009AH_uniq1.fastq /home/merce/raw_reads/1009BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1009_1.fastq
+
+cat /home/merce/raw_reads/1009AH_uniq2.fastq /home/merce/raw_reads/1009BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1009_2.fastq
+
+cat /home/merce/raw_reads/1010AH_uniq1.fastq /home/merce/raw_reads/1010BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1010_1.fastq
+
+cat /home/merce/raw_reads/1010AH_uniq2.fastq /home/merce/raw_reads/1010BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1010_2.fastq
+
+cat /home/merce/raw_reads/1011AH_uniq1.fastq /home/merce/raw_reads/1011BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1011_1.fastq
+
+cat /home/merce/raw_reads/1011AH_uniq2.fastq /home/merce/raw_reads/1011BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1011_2.fastq
+
+cat /home/merce/raw_reads/1012AH_uniq1.fastq /home/merce/raw_reads/1012BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1012_1.fastq
+
+cat /home/merce/raw_reads/1012AH_uniq2.fastq /home/merce/raw_reads/1012BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1012_2.fastq
+
+cat /home/merce/raw_reads/1001AH_uniq1.fastq /home/merce/raw_reads/1001BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1001_1.fastq
+
+cat /home/merce/raw_reads/1001AH_uniq2.fastq /home/merce/raw_reads/1001BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1001_2.fastq
+
+cat /home/merce/raw_reads/1002AH_uniq1.fastq /home/merce/raw_reads/1002BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1002_1.fastq
+
+cat /home/merce/raw_reads/1002AH_uniq2.fastq /home/merce/raw_reads/1002BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1002_2.fastq
+
+cat /home/merce/raw_reads/1003AH_uniq1.fastq /home/merce/raw_reads/1003BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1003_1.fastq
+
+cat /home/merce/raw_reads/1003AH_uniq2.fastq /home/merce/raw_reads/1003BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1003_2.fastq
+
+cat /home/merce/raw_reads/1004AH_uniq1.fastq /home/merce/raw_reads/1004BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1004_1.fastq
+
+cat /home/merce/raw_reads/1004AH_uniq2.fastq /home/merce/raw_reads/1004BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1004_2.fastq
+
+cat /home/merce/raw_reads/1005AH_uniq1.fastq /home/merce/raw_reads/1005BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1005_1.fastq
+
+cat /home/merce/raw_reads/1005AH_uniq2.fastq /home/merce/raw_reads/1005BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1005_2.fastq
+
+cat /home/merce/raw_reads/1007AH_uniq1.fastq /home/merce/raw_reads/1007BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1007_1.fastq
+
+cat /home/merce/raw_reads/1007AH_uniq2.fastq /home/merce/raw_reads/1007BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1007_2.fastq
+
+cat /home/merce/raw_reads/1008AH_uniq1.fastq /home/merce/raw_reads/1008BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1008_1.fastq
+
+cat /home/merce/raw_reads/1008AH_uniq2.fastq /home/merce/raw_reads/1008BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1008_2.fastq
+
+cat /home/merce/raw_reads/1013AH_uniq1.fastq /home/merce/raw_reads/1013BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1013_1.fastq
+
+cat /home/merce/raw_reads/1013AH_uniq2.fastq /home/merce/raw_reads/1013BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1013_2.fastq
+
+cat /home/merce/raw_reads/1014AH_uniq1.fastq /home/merce/raw_reads/1014BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1014_1.fastq
+
+cat /home/merce/raw_reads/1014AH_uniq2.fastq /home/merce/raw_reads/1014BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1014_2.fastq
+
+cat /home/merce/raw_reads/1015AH_uniq1.fastq /home/merce/raw_reads/1015BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1015_1.fastq
+
+cat /home/merce/raw_reads/1015AH_uniq2.fastq /home/merce/raw_reads/1015BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1015_2.fastq
+
+cat /home/merce/raw_reads/1016AH_uniq1.fastq /home/merce/raw_reads/1016BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1016_1.fastq
+
+cat /home/merce/raw_reads/1016AH_uniq2.fastq /home/merce/raw_reads/1016BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1016_2.fastq
+
+cat /home/merce/raw_reads/1019AH_uniq1.fastq /home/merce/raw_reads/1019BC_uniq1.fastq > /home/merce/raw_reads/fastq_uniq/1019_1.fastq
+
+cat /home/merce/raw_reads/1019AH_uniq2.fastq /home/merce/raw_reads/1019BC_uniq2.fastq > /home/merce/raw_reads/fastq_uniq/1019_2.fastq
+
