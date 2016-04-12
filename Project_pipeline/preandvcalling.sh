fa3742edf9f0282789cabab36bb4d5e1bc6f6fa7
Merce Montoliu Nerin <merce.montoliu@gmail.com>
2016-04-12 10:19:01 +0200

    take of comments from real commands

--- Project_pipeline/preandvcalling.sh
+++ Project_pipeline/preandvcalling.sh
@@ -2,41 +2,41 @@
 
 #AddOrReplaceReadGroups
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1001_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1001_RG.bam RGID=1001 RGLB=1001 RGPL=illumina RGPU=1001 RGSM=1001
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1001_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1001_RG.bam RGID=1001 RGLB=1001 RGPL=illumina RGPU=1001 RGSM=1001
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1002_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1002_RG.bam RGID=1002 RGLB=1002 RGPL=illumina RGPU=1002 RGSM=1002
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1002_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1002_RG.bam RGID=1002 RGLB=1002 RGPL=illumina RGPU=1002 RGSM=1002
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1003_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1003_RG.bam RGID=1003 RGLB=1003 RGPL=illumina RGPU=1003 RGSM=1003
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1003_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1003_RG.bam RGID=1003 RGLB=1003 RGPL=illumina RGPU=1003 RGSM=1003
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1004_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1004_RG.bam RGID=1004 RGLB=1004 RGPL=illumina RGPU=1004 RGSM=1004
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1004_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1004_RG.bam RGID=1004 RGLB=1004 RGPL=illumina RGPU=1004 RGSM=1004
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1005_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1005_RG.bam RGID=1005 RGLB=1005 RGPL=illumina RGPU=1005 RGSM=1005
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1005_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1005_RG.bam RGID=1005 RGLB=1005 RGPL=illumina RGPU=1005 RGSM=1005
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1007_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1007_RG.bam RGID=1007 RGLB=1007 RGPL=illumina RGPU=1007 RGSM=1007
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1007_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1007_RG.bam RGID=1007 RGLB=1007 RGPL=illumina RGPU=1007 RGSM=1007
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1008_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1008_RG.bam RGID=1008 RGLB=1008 RGPL=illumina RGPU=1008 RGSM=1008
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1008_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1008_RG.bam RGID=1008 RGLB=1008 RGPL=illumina RGPU=1008 RGSM=1008
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1013_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1013_RG.bam RGID=1013 RGLB=1013 RGPL=illumina RGPU=1013 RGSM=1013
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1013_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1013_RG.bam RGID=1013 RGLB=1013 RGPL=illumina RGPU=1013 RGSM=1013
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1014_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1014_RG.bam RGID=1014 RGLB=1014 RGPL=illumina RGPU=1014 RGSM=1014
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1014_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1014_RG.bam RGID=1014 RGLB=1014 RGPL=illumina RGPU=1014 RGSM=1014
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1015_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1015_RG.bam RGID=1015 RGLB=1015 RGPL=illumina RGPU=1015 RGSM=1015
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1015_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1015_RG.bam RGID=1015 RGLB=1015 RGPL=illumina RGPU=1015 RGSM=1015
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1016_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1016_RG.bam RGID=1016 RGLB=1016 RGPL=illumina RGPU=1016 RGSM=1016
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1016_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1016_RG.bam RGID=1016 RGLB=1016 RGPL=illumina RGPU=1016 RGSM=1016
 
-#java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1019_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1019_RG.bam RGID=1019 RGLB=1019 RGPL=illumina RGPU=1019 RGSM=1019
+java -jar ~/picard/dist/picard.jar AddOrReplaceReadGroups INPUT=/data02/merce/deha_vcalling/Bowtie2/1019_sorted.bam OUTPUT=/data02/merce/deha_vcalling/1019_RG.bam RGID=1019 RGLB=1019 RGPL=illumina RGPU=1019 RGSM=1019
 
 #Local realignment BAQ
 
-#samtools calmd -Arb /data02/merce/deha_vcalling/1001_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1001_RGBAQ.bam
+samtools calmd -Arb /data02/merce/deha_vcalling/1001_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1001_RGBAQ.bam
 
-#samtools calmd -Arb /data02/merce/deha_vcalling/1002_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1002_RGBAQ.bam
+samtools calmd -Arb /data02/merce/deha_vcalling/1002_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1002_RGBAQ.bam
 
-#samtools calmd -Arb /data02/merce/deha_vcalling/1003_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1003_RGBAQ.bam
+samtools calmd -Arb /data02/merce/deha_vcalling/1003_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1003_RGBAQ.bam
 
-#samtools calmd -Arb /data02/merce/deha_vcalling/1004_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1004_RGBAQ.bam
+samtools calmd -Arb /data02/merce/deha_vcalling/1004_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1004_RGBAQ.bam
 
-#samtools calmd -Arb /data02/merce/deha_vcalling/1005_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1005_RGBAQ.bam
+samtools calmd -Arb /data02/merce/deha_vcalling/1005_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1005_RGBAQ.bam
 
 samtools calmd -Arb /data02/merce/deha_vcalling/1007_RG.bam /data02/merce/Only_Debaryomyceshansenii/reference/GCA_000006445.2_ASM644v2_genomic.fna > /data02/merce/deha_vcalling/1007_RGBAQ.bam
 
@@ -54,9 +54,9 @@ samtools calmd -Arb /data02/merce/deha_vcalling/1019_RG.bam /data02/merce/Only_D
 
 #Index
 
-#samtools index /data02/merce/deha_vcalling/1001_RGBAQ.bam
+samtools index /data02/merce/deha_vcalling/1001_RGBAQ.bam
 
-#samtools index /data02/merce/deha_vcalling/1002_RGBAQ.bam
+samtools index /data02/merce/deha_vcalling/1002_RGBAQ.bam
 
 samtools index /data02/merce/deha_vcalling/1003_RGBAQ.bam
 
